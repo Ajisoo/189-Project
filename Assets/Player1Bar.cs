e4b81ef308f7737aba7be86a1b3641769588793f
Kelvin Chan <33172485+MyAngelNeptune@users.noreply.github.com>
2020-03-07 22:14:40 -0800

    basic title screen

---
+++ Assets/Player1Bar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player1Bar : MonoBehaviour
+{
+    public Image bar1;
+    public float fill;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fill = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bar1.fillAmount = fill;
+        fill += Time.deltaTime * 0.1f;
+    }
+}
